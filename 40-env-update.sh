b4285722b52d8a8e4033504940458ab137818df3
John Ipson <john@devpipeline.com>
2023-06-02 12:30:37 -0600

    Added Dockerfile and entrypoint script to configure API Urls

---
+++ 40-env-update.sh
@@ -0,0 +1,21 @@
+
+#! /bin/bash
+
+if [ -z "$API_URL" ]
+
+then
+  echo No API Environment Variable Set. Set "-e API_URL='your-server.tld'" in your Docker Config
+else
+  if [ -f /api_set ]
+  then
+    echo API URL Already set to $API_URL
+  else
+    echo Setting API URL to: $API_URL
+    sed -i "s#var apiUrl = defaultUrl#const apiUrl = '$API_URL'#" /usr/share/nginx/html/js/ide.js
+  
+    echo Disabling messages from public Judge CE API
+    sed -i "s^loadMessages();^^1" /usr/share/nginx/html/js/ide.js
+    
+    touch /api_set
+  fi
+fi
