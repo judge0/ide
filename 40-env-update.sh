b6a594ed7baaa39b2f20a2e5208cd4b7d4c85005
Genre Mamanao <41022228+glenngenre@users.noreply.github.com>
2026-08-29 00:49:01 +0800

    fix: update API URL handling in ide.js
    Signed-off-by: Genre Mamanao <41022228+glenngenre@users.noreply.github.com>

--- 40-env-update.sh
+++ 40-env-update.sh
@@ -7,12 +7,9 @@ fi
 
 echo "Setting API URL to: $API_URL"
 
-# Replace the default API URL variable in ide.js
-sed -i "s#var apiUrl = localStorageGetItem(\"api-url\") || \"https://ce.judge0.com\";#var apiUrl = \"${API_URL}\";#" /usr/share/nginx/html/js/ide.js
-sed -i "s#loadMessages();##" /usr/share/nginx/html/js/ide.js
+# Replace hardcoded judge0 URLs with your self-hosted API URL
+sed -i "s#https://ce.judge0.com#${API_URL}#g" /usr/share/nginx/html/js/ide.js
+sed -i "s#https://extra-ce.judge0.com#${API_URL}#g" /usr/share/nginx/html/js/ide.js
 
-# Hard-override local storage fallback directly at top of script
-sed -i "1s#^#localStorage.setItem('api-url', '${API_URL}');\n#" /usr/share/nginx/html/js/ide.js
-
-# Force unregister service workers
-sed -i "1s#^#if ('serviceWorker' in navigator) { navigator.serviceWorker.getRegistrations().then(rs => rs.forEach(r => r.unregister())); }\n#" /usr/share/nginx/html/js/ide.js
+# Remove external status message polling if present
+sed -i "s#loadMessages();##g" /usr/share/nginx/html/js/ide.js
